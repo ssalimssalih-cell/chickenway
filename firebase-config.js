43f088687725622fb53bb72b937340e251019b2c
ssalimssalih-cell <ssalimssalih@gmail.com>
2026-05-09 21:49:15 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,7 @@ const firebaseConfig = {
     messagingSenderId: "734739564037",
     appId: "1:734739564037:web:649d31ff5d5b561ae93e6c"
 };
-
 firebase.initializeApp(firebaseConfig);
-
 const auth = firebase.auth();
 const db = firebase.firestore();
-
 console.log('✅ Firebase OK');
