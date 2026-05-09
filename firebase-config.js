cc90c80ad20e6ab45755a59e130d7b7605ed46f8
ssalimssalih-cell <ssalimssalih@gmail.com>
2026-05-09 23:16:20 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-// ============================================
-// FIREBASE CONFIGURATION - CHICKEN WAY
-// ============================================
-
 const firebaseConfig = {
     apiKey: "AIzaSyDBtroF6W2tgAmJeGwtSCjNGeYcG77IfsU",
     authDomain: "chickenway2026.firebaseapp.com",
@@ -11,11 +7,9 @@ const firebaseConfig = {
     appId: "1:734739564037:web:649d31ff5d5b561ae93e6c"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Global references
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-console.log('✅ Firebase initialized - Chicken Way');
+console.log('Firebase OK');
