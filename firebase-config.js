3e9d9f9ac7cf29763b6d70994caa5fe568e41241
ssalimssalih-cell <ssalimssalih@gmail.com>
2026-06-10 16:33:05 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 if (typeof firebaseConfig === 'undefined') {
-    const firebaseConfig = {
-        apiKey: "AIzaSyDBtroF6W2tgAmJeGwtSCjNGeYcG77IfsU",
-        authDomain: "chickenway2026.firebaseapp.com",
-        projectId: "chickenway2026",
-        storageBucket: "chickenway2026.firebasestorage.app",
-        messagingSenderId: "734739564037",
-        appId: "1:734739564037:web:649d31ff5d5b561ae93e6c"
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSy...",           // ← une vraie clé, pas "VOTRE_API_KEY"
+  authDomain: "alma-coffee-shop.firebaseapp.com",
+  projectId: "alma-coffee-shop",
+  storageBucket: "alma-coffee-shop.firebasestorage.app",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:..."
+};
     firebase.initializeApp(firebaseConfig);
     var auth = firebase.auth();
     var db = firebase.firestore();
